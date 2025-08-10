3053db10200c5f1aa24770a93ecd84b8d172d3b8
Gauri Daulatkar <gauridaulatkar@gmail.com>
2025-08-11 00:04:13 +0530

    Added 2 conditional statement programs

---
+++ ConditionalStatements/Evenorodd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Evenorodd {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        int n=sc.nextInt();
+
+        if(n%2==0){
+            System.out.println("Entered Number is Even");
+        }
+
+        else{
+            System.out.println("Number is odd");
+        }
+
+
+    }
+    
+}
