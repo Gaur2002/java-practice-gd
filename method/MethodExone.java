3f2541d21b56dfef4796336b3f978e6ae5e8f008
Gauri Daulatkar <gauridaulatkar@gmail.com>
2025-08-20 12:52:08 +0530

    Added 2 new programs in method folder

---
+++ method/MethodExone.java
@@ -0,0 +1,9 @@
+//Static-void  
+public class MethodExone {
+    public static void m1(){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args){
+        m1();
+    }   
+}
---
+++ method/MethodExtwo.java
@@ -0,0 +1,11 @@
+//Non-static-void
+public class MethodExtwo {
+    public void m1(){
+        System.out.println("Hello");
+    }
+    public static void main(String args[]){
+       MethodExtwo obj=new MethodExtwo();
+       obj.m1();
+
+    }    
+}
