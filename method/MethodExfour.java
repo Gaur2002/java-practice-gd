2a4da01a2dc5e8365a8677566048fe4cecfaa15e
Gauri Daulatkar <gauridaulatkar@gmail.com>
2025-08-24 17:35:45 +0530

    Added 2 more programs in method folder

---
+++ method/MethodExfour.java
@@ -0,0 +1,12 @@
+//Non-Static-return type
+public class MethodExfour{
+    public String m1(){
+        return "hello";
+    }
+    public static void main(String[] args){
+            MethodExfour obj=new MethodExfour();
+            obj.m1();
+
+
+    }
+}
---
+++ method/MethodExthree.java
@@ -0,0 +1,9 @@
+//Static-return type
+public class MethodExthree{
+    public static String m1(){
+        return "hello";
+    }
+    public static void main(String[] args){
+       System.out.println(m1());// or String return=m1();
+    }                           //    S.o.p(result);
+}
